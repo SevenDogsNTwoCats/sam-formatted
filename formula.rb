9463f98d0c36da3e3b7d9fbe7ecc51aa46db6811
Alhanis Espinal <alhanis.espinal@ginih.com>
2025-06-27 15:07:28 -0600

    Reintroduce Homebrew formula for sam-formatted tool with updated dependencies and installation instructions

--- formula.rb
+++ formula.rb
@@ -3,10 +3,10 @@ class SamFormatted < Formula
   desc "A command-line tool for formatting and displaying logs from AWS SAM"
   homepage "https://github.com/SevenDogsNTwoCats/sam-formatted"
   url "https://github.com/SevenDogsNTwoCats/sam-formatted/archive/v1.0.1.tar.gz"
-  sha256 "YOUR_TARBALL_SHA256"
+  sha256 "f9703711e0e001a173371822a8cee9302e63dc6dce8913aa67acf6b46e393bf5"
   license "MIT"
 
-  depends_on "jq"
+  depends_on "jq", "sam-cli", "node", "npm", "nodemon", "awscli"
 
   def install
     bin.install "bin/sam-formatted.sh" => "sam-formatted"
